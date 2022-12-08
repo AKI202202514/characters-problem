b9748313deedb6cf234ccc43a62dd671aef6678a
Uthman Akinwumi <AKI202202514@longroad.ac.uk>
2022-12-08 12:19:33 +0000

    Add project files.

---
+++ characters problem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace characters_problem
+{
+    internal class Program
+    {
+        // Subroutine to output alphanumerics
+        static void Characters()
+        {
+            string Digits = "01234567890";
+            string Characters = "abcdABCD@#!£";
+            Console.WriteLine("The digits are: " + Digits);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
